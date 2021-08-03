8c91320e5ad2bf09d83a6e36db7f82cd1991b661
mao <michaelaorndorff@gmail.com>
2021-08-03 15:00:35 -0400

    idk

--- arch2.sh
+++ arch2.sh
@@ -3,12 +3,12 @@
 echo "this is arch2.sh you are chrooted"
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
-echo "en_US.UTF-8 UTF-8  " >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8  " > /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 read PCNAME
-echo $PCNAME >> /etc/hostname
+echo $PCNAME > /etc/hostname
 
 passwd
 
